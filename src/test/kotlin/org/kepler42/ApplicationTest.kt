499f91f85d98a1c3ec84e165c1e2e41f6ef295a4
Henrique Rocha <henriquerocha@tutanota.com>
2021-10-25 15:33:34 -0400

    feat: change world to kepler

--- src/test/kotlin/org/kepler42/ApplicationTest.kt
+++ src/test/kotlin/org/kepler42/ApplicationTest.kt
@@ -17,7 +17,7 @@ class ApplicationTest {
         withTestApplication({ configureRouting() }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello World!", response.content)
+                assertEquals("Hello Kepler!", response.content)
             }
         }
     }
