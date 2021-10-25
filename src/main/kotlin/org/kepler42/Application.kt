0a9b0fdbcd4bc3422e14c649b435b32bade7e8be
Henrique Rocha <henriquerocha@tutanota.com>
2021-10-25 15:28:16 -0400

    feat: first commit

---
+++ src/main/kotlin/org/kepler42/Application.kt
@@ -0,0 +1,13 @@
+package org.kepler42
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.kepler42.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+        configureHTTP()
+    }.start(wait = true)
+}
