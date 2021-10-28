9234b53be9d26994759b65a05e3b8798ecfeceae
Henrique Rocha <henriquerocha@tutanota.com>
2021-10-28 16:40:36 -0300

    feat: add datbase configuration

--- src/main/kotlin/org/kepler42/Application.kt
+++ src/main/kotlin/org/kepler42/Application.kt
@@ -2,12 +2,16 @@ package org.kepler42
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+
 import org.kepler42.plugins.*
+import org.kepler42.database.*
+
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureSerialization()
         configureHTTP()
+        configureDatabase()
     }.start(wait = true)
 }
---
+++ src/main/kotlin/org/kepler42/database/DatabaseConfiguration.kt
@@ -0,0 +1,28 @@
+package org.kepler42.database
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+
+import io.ktor.application.*
+
+import org.kepler42.database.Greeting
+
+fun Application.configureDatabase() {
+    createConnection()
+    createTable()
+}
+
+fun createConnection() {
+    Database.connect(
+        "jdbc:pgsql://localhost:5432/postgres",
+        driver = "com.impossibl.postgres.jdbc.PGDriver",
+        user = "postgres",
+        password = "postgres")
+}
+
+fun createTable() {
+    transaction {
+        addLogger(StdOutSqlLogger)
+        SchemaUtils.create(Greeting)
+    }
+}
