a096f9fdb6a7bae82b5ec650032989fbabb1f1e1
SidBertalia <sidbertalia@gmail.com>
2022-01-04 12:03:16 -0300

    test: exception user name exceeds 200 chars

--- src/test/kotlin/org/kepler42/controllers/UserControllerTest.kt
+++ src/test/kotlin/org/kepler42/controllers/UserControllerTest.kt
@@ -1,7 +1,7 @@
 package org.kepler42.controllers
 
+import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrowExactly
-import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
@@ -9,6 +9,12 @@ import org.kepler42.errors.InvalidNameException
 import org.kepler42.models.User
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+
+fun generateString(length: Int): String {
+    val charset = ('a' .. 'z')
+    return List(length) { charset.random() }
+        .joinToString("")
+}
 
 object UserControllerTest : Spek({
     val fakeUserRepository = mockk<UserRepository>()
@@ -24,8 +30,16 @@ object UserControllerTest : Spek({
         }
 
         it("doesn't throw when name have 2 or more letters") {
-            val user = controller.createUser(User(name = "Ada"))
-            user.name shouldBe "Ada"
+            shouldNotThrow<InvalidNameException> {
+                controller.createUser(User(name = "Ada"))
+            }
+        }
+
+        it("throws when user name has more than 200 letters") {
+            val bigName = generateString(201)
+            shouldThrowExactly<InvalidNameException> {
+                controller.createUser(User(name = bigName))
+            }
         }
     }
 })
