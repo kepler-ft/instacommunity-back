74d95b0c240ee200afb68e462b37cd01343f52b9
SidBertalia <sidbertalia@gmail.com>
2022-01-06 12:00:45 -0300

    feat: add more info to user model

--- src/main/kotlin/org/kepler42/models/User.kt
+++ src/main/kotlin/org/kepler42/models/User.kt
@@ -2,4 +2,13 @@ package org.kepler42.models
 
 import kotlinx.serialization.Serializable
 
-@Serializable data class User(val id: Int = 0, val name: String? = null)
+@Serializable data class User(
+    val id: Int = 0,
+    val googleId: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val nickName: String? = null,
+    val jobPost: String? = null,
+    val usePhoto: Boolean? = null,
+    val email: String? = null,
+)
--- src/main/resources/db/migration/V1.0__InitialVersion.sql
+++ src/main/resources/db/migration/V1.0__InitialVersion.sql
@@ -1,6 +1,11 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL
+    firstName VARCHAR NOT NULL,
+    lastName VARCHAR NOT NULL,
+    nickName VARCHAR NOT NULL,
+    jobPost VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    usePhoto BOOLEAN
 );
 
 CREATE TABLE communities (
@@ -22,8 +27,8 @@ CREATE TABLE users_communities (
     UNIQUE (user_id, community_id)
 );
 
-INSERT INTO users(name)
-VALUES ('Ada');
+INSERT INTO users(googleId, firstName, lastName, nickName, jobPost, email, usePhoto)
+VALUES ('batatinhafrita123', 'Ada', 'Luvlace', 'Ada', 'Programadora', 'ada@example.com', false);
 
 INSERT INTO communities(name, contact, description, creator)
 VALUES ('Getting Started', 'https://github.com/kepler-ft',
@@ -31,7 +36,7 @@ VALUES ('Getting Started', 'https://github.com/kepler-ft',
         'é uma estrela anã vermelha localizada na constelação de ' ||
         'Cygnus a cerca de 126 anos-luz a partir do Sol. ' ||
         'Ela tem três planetas extrassolares conhecidos, ' ||
-        'os quais tem raios menores do que o raio da Terra,[1] e, ' ||
+        'os quais tem raios menores do que o raio da Terra, e, ' ||
         'provavelmente, também são menores em massa.', 1);
 
 INSERT INTO users_communities(user_id, community_id)
