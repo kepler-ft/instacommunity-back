eaa8a4af94260cafc15ed57d652e5fde90dfed9b
Paulo Santana <paulo.santana.r@gmail.com>
2022-01-10 12:10:41 -0300

    fix: use DSL syntax to insert user

--- src/main/kotlin/org/kepler42/database/repositories/UserRepositoryImpl.kt
+++ src/main/kotlin/org/kepler42/database/repositories/UserRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package org.kepler42.database.repositories
 
+import org.jetbrains.exposed.dao.id.EntityID
 import org.kepler42.models.User
 import org.kepler42.database.entities.UserEntity
 import org.kepler42.controllers.UserRepository
@@ -8,6 +9,7 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.kepler42.database.entities.CommunityEntity
 import org.kepler42.database.entities.Users
+import org.kepler42.database.entities.Users.id
 import org.kepler42.models.Community
 
 class UserRepositoryImpl: UserRepository {
@@ -20,17 +22,18 @@ class UserRepositoryImpl: UserRepository {
     }
 
     override fun insertUser(user: User): User {
-        val newUser = transaction {
+        transaction {
             addLogger(StdOutSqlLogger)
-            UserEntity.new {
-                name = user.name!!
-                username = user.username!!
-                email = user.email!!
-                occupation = user.occupation!!
-                usePhoto = user.usePhoto!!
+            Users.insert {
+                it[id] = user.id
+                it[name] = user.name!!
+                it[username] = user.username!!
+                it[email] = user.email!!
+                it[occupation] = user.occupation!!
+                it[usePhoto] = user.usePhoto!!
             }
         }
-        return newUser.toModel()
+        return user
     }
 
     override fun changeUser(user: User): User? {
