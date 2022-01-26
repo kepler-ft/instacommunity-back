7d7164eec29323bda4c96d1dc40044509fcbec48
Paulo Santana <paulo.santana.r@gmail.com>
2022-01-26 12:33:19 -0300

    test: get a list of all tags

--- src/test/kotlin/org/kepler42/routes/TagsRouteTest.kt
+++ src/test/kotlin/org/kepler42/routes/TagsRouteTest.kt
@@ -1,2 +1,59 @@
 package org.kepler42.routes
 
+import io.kotest.matchers.shouldBe
+import io.ktor.application.*
+import io.ktor.http.*
+import io.mockk.*
+import io.ktor.server.testing.*
+import kotlinx.serialization.json.Json
+import org.kepler42.controllers.CommunityController
+import org.kepler42.controllers.TagController
+import org.kepler42.database.repositories.TagRepository
+import org.kepler42.plugins.configureRouting
+import org.kepler42.plugins.configureSerialization
+import org.kepler42.models.Tag
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import org.kepler42.testUtils.*
+import org.koin.dsl.module
+import org.koin.ktor.ext.Koin
+
+
+fun generateTags(): List<Tag> {
+    return listOf(
+        Tag(1, "Dev"),
+        Tag(2, "DevOps"),
+        Tag(3, "Lazer"),
+    )
+}
+
+object TagsRouteTest : Spek({
+
+    val fakeTagRepository = mockk<TagRepository>()
+    fun setup(app: Application) {
+
+        val testKoinModule = module {
+            single {TagController(fakeTagRepository) }
+        }
+        app.install(Koin) {
+            modules(testKoinModule)
+        }
+        app.configureRouting()
+        app.configureSerialization()
+    }
+
+    describe("Tag route") {
+        it("should get all tags") {
+            withTestApplication({ setup(this) }) {
+                val tagList = generateTags()
+                every { fakeTagRepository.getAll() } answers { tagList }
+                handleRequest(HttpMethod.Get, "/tags").apply {
+
+                    response.status() shouldBe HttpStatusCode.OK
+                    val tagListResult = Json.decodeFromString<List<Tag>>(response.content!!)
+                    tagListResult shouldBe tagList
+                }
+            }
+        }
+    }
+})
